c50330bed92abc44a39b3a4ff918712f29f3146a
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-26 00:16:08 -0400

    brute-force method

--- ArrayMaxProduct/Program.cs
+++ ArrayMaxProduct/Program.cs
@@ -10,6 +10,47 @@ namespace ArrayMaxProduct
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("\nInput an array of integer\n\n>>> ");
+                    string[] input = Console.ReadLine().Split(',');
+                    int[] intArr = new int[input.Length];
+                    for (int i = 0; i < input.Length; i++)
+                    {
+                        intArr[i] = Convert.ToInt32(input[i]);
+                    }
+                    Console.WriteLine($"\nMaxProduct: {MaxProduct(intArr)}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"\n{ex.Message}");
+                }
+                Console.Write("\nPress Enter to try another string...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static int MaxProduct(int[] arr)
+        {
+            int max1 = arr[0] > arr[1] ? arr[0] : arr[1];
+            int max2 = arr[0] > arr[1] ? arr[1] : arr[0];
+
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i] > max1 && arr[i] > max2)
+                {
+                    max2 = max1;
+                    max1 = arr[i];
+                }
+                else if (arr[i] > max2)
+                {
+                    max2 = arr[i];
+                }
+            }
+            return max1 * max2;
         }
     }
 }
